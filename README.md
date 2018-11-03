615975b0794d5d66fce7355fb2b3b3e281883a8e
marcosjrs <marcosjrs80@gmail.com>
2018-11-03 09:32:36 +0100

    .

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.3.
 
+## Links interesantes para la inserción de un login con Auth0
+
+Como añadir el Login con Angular:
+```
+https://auth0.com/docs/quickstart/spa/angular2
+``` 
+
+Como configurar el login:
+```
+https://auth0.com/docs/libraries/lock/v11/configuration
+```
+## Notas
+
+Recordar que hay que añadir los datos del desarrollador (clientID, domain y audience) en el auth.service.ts
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { APP_ROUTING } from "./app.routes";
+
+// servicios
+import { AuthService } from "./services/auth.service";
+import { AuthGuardService }  from "./services/auth-guard.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PreciosComponent,
+    ProtegidaComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
